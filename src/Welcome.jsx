5a8f0913378b1a6f80ceb31a443f72efe2708f3e
Prem Patel <prem.p1@ahduni.edu.in>
2024-08-03 22:33:24 +0530

    project almost completed

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from 'react-router-dom';
 import './welcomeHome.css'
 import { useLocation } from 'react-router-dom';
-import gtaVideo from './video/gtaVideo.mp4';
+import gtaTrailer from './video/gtaTrailer.mp4';
 
 
 export default function Welcome(){
@@ -21,7 +21,7 @@ export default function Welcome(){
 
     return <div className="welcomePage">
         <video autoPlay loop muted className='GTA5Trailer'>
-            <source src={gtaVideo} type='video/mp4' />
+            <source src={gtaTrailer} type='video/mp4' />
         </video>
         <div className="welcomePara"><p className="p1">Welcome to <span className="red">Los Santos</span>! The most notorious trio, <span className="red">Michael</span>, <span className="red">Franklin</span> and <span className="red">Trevor</span> are ready to</p><p className="p2">chat and bring the streets of Los Santos to life. Dive into their world and explore the city</p><p className="p3">through their eyes. Let's <span className="red">get started</span> on this epic adventure! Who will you talk to first?</p></div>
         <div className="startWithCharacter">
