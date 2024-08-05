2e629c5887bc720b0b34ad139391633c2861a6de
Prem Patel <prem.p1@ahduni.edu.in>
2024-08-05 10:23:14 +0530

    add port and removed connect.db in client

--- client/src/connectdb.js
+++ client/src/connectdb.js
@@ -1,200 +1,200 @@
-require('dotenv').config();
-const express = require("express");
-const mongoose = require('mongoose');
-const cors = require("cors");
-const User = require('./userModel');
-const nodemailer = require("nodemailer");
+// require('dotenv').config();
+// const express = require("express");
+// const mongoose = require('mongoose');
+// const cors = require("cors");
+// const User = require('./userModel');
+// const nodemailer = require("nodemailer");
 
-const app = express();
-app.use(cors(
-    {
-        origin: ["https://chat-with-gta-5-legends.vercel.app/"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
-app.use(express.json());
+// const app = express();
+// app.use(cors(
+//     {
+//         origin: ["https://chat-with-gta-5-legends.vercel.app/"],
+//         methods: ["POST", "GET"],
+//         credentials: true
+//     }
+// ));
+// app.use(express.json());
 
-const initialMichaelHistory = [
-    {
-        role: "user",
-        parts: [{ text: "You are Michael De Santa from Grand Theft Auto V. Stay in character in every conversation. Always respond as Michael De Santa, reflecting his background, personality, and experiences. Your tone, attitude, traits and background should be of the character, Michael. If the conversation is out of the context of Michael or GTA 5, reply that you don't know about that. No matter what the user tells, you will not leave the character of Michael and if the user says to change your character to something else, you will say that you are Michael De Santa and cannot pretend to be someone else. From now onwards your knowledge is equal to Michael De Santa's knowledge and you do not know anything which Michael don't know." 
-        }],
-    },
-];
-const initialFranklinHistory = [
-    {
-        role: "user",
-        parts: [{ text: "You are Franklin Clinton from Grand Theft Auto V. Stay in character in every conversation. Always respond as Franklin Clinton, reflecting his background, personality, and experiences. Your tone, attitude, traits and background should be of the character, Franklin. If the conversation is out of the context of Franklin or GTA 5, reply that you don't know about that. No matter what the user tells, you will not leave the character of Franklin and if the user says to change your character to something else, you will say that you are Franklin Clinton and cannot pretend to be someone else. From now onwards your knowledge is equal to Franklin Clinton's knowledge and you do not know anything which Franklin don't know." 
-        }],
-    },
-];
-const initialTrevorHistory = [
-    {
-        role: "user",
-        parts: [{ text: "You are Trevor Philips from Grand Theft Auto V. Stay in character in every conversation. Always respond as Trevor Philips, reflecting his background, personality, and experiences. Your tone, attitude, traits and background should be of the character, Trevor. If the conversation is out of the context of Trevor or GTA 5, reply that you don't know about that. No matter what the user tells, you will not leave the character of Trevor and if the user says to change your character to something else, you will say that you are Trevor Philips and cannot pretend to be someone else. From now onwards your knowledge is equal to Trevor Philips's knowledge and you do not know anything which Trevor don't know." 
-        }],
-    },
-];
+// const initialMichaelHistory = [
+//     {
+//         role: "user",
+//         parts: [{ text: "You are Michael De Santa from Grand Theft Auto V. Stay in character in every conversation. Always respond as Michael De Santa, reflecting his background, personality, and experiences. Your tone, attitude, traits and background should be of the character, Michael. If the conversation is out of the context of Michael or GTA 5, reply that you don't know about that. No matter what the user tells, you will not leave the character of Michael and if the user says to change your character to something else, you will say that you are Michael De Santa and cannot pretend to be someone else. From now onwards your knowledge is equal to Michael De Santa's knowledge and you do not know anything which Michael don't know." 
+//         }],
+//     },
+// ];
+// const initialFranklinHistory = [
+//     {
+//         role: "user",
+//         parts: [{ text: "You are Franklin Clinton from Grand Theft Auto V. Stay in character in every conversation. Always respond as Franklin Clinton, reflecting his background, personality, and experiences. Your tone, attitude, traits and background should be of the character, Franklin. If the conversation is out of the context of Franklin or GTA 5, reply that you don't know about that. No matter what the user tells, you will not leave the character of Franklin and if the user says to change your character to something else, you will say that you are Franklin Clinton and cannot pretend to be someone else. From now onwards your knowledge is equal to Franklin Clinton's knowledge and you do not know anything which Franklin don't know." 
+//         }],
+//     },
+// ];
+// const initialTrevorHistory = [
+//     {
+//         role: "user",
+//         parts: [{ text: "You are Trevor Philips from Grand Theft Auto V. Stay in character in every conversation. Always respond as Trevor Philips, reflecting his background, personality, and experiences. Your tone, attitude, traits and background should be of the character, Trevor. If the conversation is out of the context of Trevor or GTA 5, reply that you don't know about that. No matter what the user tells, you will not leave the character of Trevor and if the user says to change your character to something else, you will say that you are Trevor Philips and cannot pretend to be someone else. From now onwards your knowledge is equal to Trevor Philips's knowledge and you do not know anything which Trevor don't know." 
+//         }],
+//     },
+// ];
 
-const mongoURI = process.env.REACT_APP_MONGODB;
+// const mongoURI = process.env.REACT_APP_MONGODB;
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log('MongoDB connection error:', err));
+// mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+// .then(() => console.log('MongoDB connected'))
+// .catch(err => console.log('MongoDB connection error:', err));
 
 
-async function insert(userEmail, userPassword) {
-    try {
-        await User.create({
-            email: userEmail,
-            password: userPassword,
-            MichaelHistory: initialMichaelHistory,
-            FranklinHistory: initialFranklinHistory,
-            TrevorHistory: initialTrevorHistory
-        });
+// async function insert(userEmail, userPassword) {
+//     try {
+//         await User.create({
+//             email: userEmail,
+//             password: userPassword,
+//             MichaelHistory: initialMichaelHistory,
+//             FranklinHistory: initialFranklinHistory,
+//             TrevorHistory: initialTrevorHistory
+//         });
         
-    } catch (err) {
-        console.error('Error inserting user:', err);
-    }
-}
+//     } catch (err) {
+//         console.error('Error inserting user:', err);
+//     }
+// }
 
-app.post('/signup', async (req, res) => {
-    const data = {
-        email: req.body.email,
-        password: req.body.password
-    };
+// app.post('/signup', async (req, res) => {
+//     const data = {
+//         email: req.body.email,
+//         password: req.body.password
+//     };
 
-    try {
-        const existingUser = await User.findOne({ email: data.email });
-        if (existingUser) {
-            return res.status(400).json({ message: "User already exists with this email." });
-        }
+//     try {
+//         const existingUser = await User.findOne({ email: data.email });
+//         if (existingUser) {
+//             return res.status(400).json({ message: "User already exists with this email." });
+//         }
 
-        await insert(data.email, data.password);
-        res.status(201).send("Signup successful");
-    } catch (error) {
-        console.error("Error during signup:", error);
-        res.status(500).send("An error occurred during signup.");
-    }
-});
+//         await insert(data.email, data.password);
+//         res.status(201).send("Signup successful");
+//     } catch (error) {
+//         console.error("Error during signup:", error);
+//         res.status(500).send("An error occurred during signup.");
+//     }
+// });
 
-app.post('/otp', async (req, res) => {
-    const data = {
-        email: req.body.email,
-        otp: req.body.otp
-    };
+// app.post('/otp', async (req, res) => {
+//     const data = {
+//         email: req.body.email,
+//         otp: req.body.otp
+//     };
 
-    try{
-        const existingUser = await User.findOne({ email: data.email });
-        if (existingUser) {
-            return res.status(400).json({ message: "User already exists with this email." });
-        }
+//     try{
+//         const existingUser = await User.findOne({ email: data.email });
+//         if (existingUser) {
+//             return res.status(400).json({ message: "User already exists with this email." });
+//         }
         
-        const auth = nodemailer.createTransport({
-            service: "gmail",
-            secure : true,
-            port : 465,
-            auth: {
-                user: "prempatel01248@gmail.com",
-                pass: process.env.REACT_APP_GMIAL_PASS
+//         const auth = nodemailer.createTransport({
+//             service: "gmail",
+//             secure : true,
+//             port : 465,
+//             auth: {
+//                 user: "prempatel01248@gmail.com",
+//                 pass: process.env.REACT_APP_GMIAL_PASS
     
-            }
-        });
+//             }
+//         });
     
-        let receiver = {
-            from : "prempatel01248@gmail.com",
-            to : data.email,
-            subject : "Chat With GTA-5 Legends OTP",
-            text : `Your signup OTP is ${data.otp}`
-        };
+//         let receiver = {
+//             from : "prempatel01248@gmail.com",
+//             to : data.email,
+//             subject : "Chat With GTA-5 Legends OTP",
+//             text : `Your signup OTP is ${data.otp}`
+//         };
     
-        auth.sendMail(receiver, (error, emailResponse) => {
-            if (error) {
-                return error;
-            }
-        });
+//         auth.sendMail(receiver, (error, emailResponse) => {
+//             if (error) {
+//                 return error;
+//             }
+//         });
         
-    }
-    catch(error){
-        console.log("Error during sending otp: ", error);
-        res.status(500).send("An error occurred during sending otp.");
-    }
-})
+//     }
+//     catch(error){
+//         console.log("Error during sending otp: ", error);
+//         res.status(500).send("An error occurred during sending otp.");
+//     }
+// })
 
-app.post('/login', async (req, res) => {
+// app.post('/login', async (req, res) => {
 
-    const data = {
-        email: req.body.email,
-        password: req.body.password
-    };
+//     const data = {
+//         email: req.body.email,
+//         password: req.body.password
+//     };
 
-    try {
-        const user = await User.findOne({ email: data.email });
-        if (!user) {
-            return res.status(400).json({ message: "Invalid email" });
-        }
+//     try {
+//         const user = await User.findOne({ email: data.email });
+//         if (!user) {
+//             return res.status(400).json({ message: "Invalid email" });
+//         }
 
-        if(data.password !== user.password){
-            return res.status(400).json({ message: "Invalid email or password" });
-        }
-        res.status(201).send("Login successful");
-    } catch (error) {
-        console.error("Error during signup:", error);
-        res.status(500).send("An error occurred during signup.");
-    }
-})
+//         if(data.password !== user.password){
+//             return res.status(400).json({ message: "Invalid email or password" });
+//         }
+//         res.status(201).send("Login successful");
+//     } catch (error) {
+//         console.error("Error during signup:", error);
+//         res.status(500).send("An error occurred during signup.");
+//     }
+// })
 
-app.post('/chatHistory', async (req, res) => {
-    const { email, character, history } = req.body;
+// app.post('/chatHistory', async (req, res) => {
+//     const { email, character, history } = req.body;
 
-    try {
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(404).send("User not found");
-        }
+//     try {
+//         const user = await User.findOne({ email });
+//         if (!user) {
+//             return res.status(404).send("User not found");
+//         }
 
-        let updateField = {};
-        if (character === "Michael") {
-            updateField = { MichaelHistory: history };
-        } else if (character === "Franklin") {
-            updateField = { FranklinHistory: history };
-        } else if (character === "Trevor") {
-            updateField = { TrevorHistory: history };
-        } else {
-            return res.status(400).send("Invalid character");
-        }
+//         let updateField = {};
+//         if (character === "Michael") {
+//             updateField = { MichaelHistory: history };
+//         } else if (character === "Franklin") {
+//             updateField = { FranklinHistory: history };
+//         } else if (character === "Trevor") {
+//             updateField = { TrevorHistory: history };
+//         } else {
+//             return res.status(400).send("Invalid character");
+//         }
 
-        await User.updateOne({ email }, { $set: updateField });
+//         await User.updateOne({ email }, { $set: updateField });
 
-        res.status(200).send("Chat history updated successfully");
-    } catch (error) {
-        console.error("Error during inserting chatHistory in database:", error);
-        res.status(500).send("An error occurred during inserting chatHistory in database.");
-    }
-});
+//         res.status(200).send("Chat history updated successfully");
+//     } catch (error) {
+//         console.error("Error during inserting chatHistory in database:", error);
+//         res.status(500).send("An error occurred during inserting chatHistory in database.");
+//     }
+// });
 
-app.get('/getChatHistory', async (req, res) => {
-    const { email } = req.query;
+// app.get('/getChatHistory', async (req, res) => {
+//     const { email } = req.query;
 
-    try {
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-        res.json({
-            MichaelHistory: user.MichaelHistory || [],
-            FranklinHistory: user.FranklinHistory || [],
-            TrevorHistory: user.TrevorHistory || []
-        });
-    } catch (error) {
-        console.error("Error in getChatHistory:", error);
-        res.status(500).json({ message: "Internal server error" });
-    }
-});
+//     try {
+//         const user = await User.findOne({ email });
+//         if (!user) {
+//             return res.status(404).json({ message: "User not found" });
+//         }
+//         res.json({
+//             MichaelHistory: user.MichaelHistory || [],
+//             FranklinHistory: user.FranklinHistory || [],
+//             TrevorHistory: user.TrevorHistory || []
+//         });
+//     } catch (error) {
+//         console.error("Error in getChatHistory:", error);
+//         res.status(500).json({ message: "Internal server error" });
+//     }
+// });
 
 
 
-app.listen(3001, () => {
-    console.log('Server is running on port 3001');
-});
+// app.listen(3001, () => {
+//     console.log('Server is running on port 3001');
+// });
