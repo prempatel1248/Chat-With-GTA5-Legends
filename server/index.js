64fa63f4c467cbcc90c88266af012ab2b16738e5
prempatel1248 <141221885+prempatel1248@users.noreply.github.com>
2024-12-27 19:05:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -204,6 +204,6 @@ app.get('/getChatHistory', async (req, res) => {
 
 
 
-app.listen(3001, () => {
+app.listen('https://chat-with-gta5-legends.onrender.com', () => {
     console.log('Server is running on port 3001');
 });
